ec7b1a457547e84e57d3d3702cb89de70feaebb5
Andrew Meshekoff <meshekoff@gmail.com>
2015-03-05 15:05:00 -0800

    syscall handler done

--- project_2/pintos/src/userprog/syscall.c
+++ project_2/pintos/src/userprog/syscall.c
@@ -15,6 +15,125 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+	int call_num = (*(int *)f -> esp);
+	void * argv[3];
+	if (call_num == SYS_CREATE || call_num == SYS_SEEK) { // 2 arguments
+		for (int i = 0; i<2; i++) {
+			void * arg_ptr = (void *) f -> esp + sizeof(uint32_t) * (i+1);
+			validate_ptr(arg_ptr);
+			argv[i] = arg_ptr;
+		}
+	}
+	else if (call_num == SYS_READ || call_num == SYS_WRITE) { // 3 arguments
+		for (int i = 0; i<3; i++) {
+			void * arg_ptr = (void *) f -> esp + sizeof(uint32_t) * (i+1);
+			validate_ptr(arg_ptr);
+			argv[i] = arg_ptr;
+		}
+	}
+	else if (0 <= call_num <= 12) { // 1 argument
+		void * arg_ptr = f -> esp + sizeof(uint32_t);
+		validate_ptr(arg_ptr);
+		argv[0] = arg_ptr;
+	}
+	else
+		// error!
+
+	switch (call_num) {
+		case SYS_HALT:
+			sys_halt();
+		break;
+		case SYS_EXIT:
+			sys_exit(*(int *) argv[0]);
+		break;
+		case SYS_EXEC:
+			sys_exec(*(int *) argv[0]);
+		break;
+		case SYS_WAIT:
+			sys_wait(*(pid_t *) argv[0]);
+		break;
+		case SYS_CREATE:
+			sys_create(*(char **) argv[0], *(unsigned *)argv[1])
+		break;
+		case SYS_REMOVE:
+			sys_remove(*(char **) argv[0]);
+		break;
+		case SYS_OPEN:
+			sys_open(*(char **) argv[0]);
+		break;
+		case SYS_FILESIZE:
+			sys_filesize(*(int *) argv[0]);
+		break;
+		case SYS_READ:
+			sys_read(*(int *) argv[0], *(argv[1]), *(unsigned *) argv[2]);
+		break;
+		case SYS_WRITE:
+			sys_write(*(int *) argv[0], *(argv[1]), *(unsigned *) argv[2]);
+		break;
+		case SYS_SEEK:
+			sys_seek(*(int *) argv[0], *(unsigned *) argv[1]);
+		break;
+		case SYS_TELL:
+			sys_tell(*(int *) argv[0]);
+		break;
+		case SYS_CLOSE:
+			sys_close(*(int *) argv[0]);
+		break;
+		default:
+			//An error occured. Do something.
+		break;
+	}
 }
+
+void sys_halt (void) {
+
+}
+
+void sys_exit (int status) {
+
+}
+
+pid_t sys_exec (const char *file) {
+	return 0; //replace this with something usefull
+}
+
+int sys_wait (pid_t pid) {
+	return 0; //replace this with something usefull
+}
+
+bool sys_create (const char *file, unsigned initial_size) {
+	return 0; //replace this with something usefull
+}
+
+bool sys_remove (const char *file) {
+	return 0; //replace this with something usefull
+}
+
+int sys_open (const char *file) {
+	return 0; //replace this with something usefull
+}
+
+int sys_filesize (int fd) {
+	return 0; //replace this with something usefull
+}
+
+int sys_read (int fd, void *buffer, unsigned size) {
+	return 0; //replace this with something usefull
+}
+
+int sys_write (int fd, const void *buffer, unsigned size) {
+	return 0; //replace this with something usefull
+}
+
+void sys_seek (int fd, unsigned position) {
+
+}
+
+unsigned sys_tell (int fd) {
+	return 0; //replace this with something usefull
+}
+
+void sys_close (int fd) {
+
+}
+
